e573d3f00a354c49e0b7f686eadbdac47aa0035a
yesmin <ymslahoud@gmail.com>
2020-02-23 09:17:55 -0300

    feat: add search term and prefix

---
+++ index.js
@@ -0,0 +1,33 @@
+const readline = require('readline-sync')
+
+function start() {
+    const content = {}
+
+    //termo de busca
+    content.searchTerm = askAndReturnSearchTerm()
+    //esse prefixo vai ser utilizado quando fizer o upload no YouTube
+    //para o título ficar mais "humanamente" legível
+    content.prefix = askAndReturnPrefix()
+
+    function askAndReturnSearchTerm() {
+        //vai rodar o método question da biblioteca readline 
+        //e vai retornar o resultado (o input do usuário)
+        //e injeta na propriedade searchTerm
+        return readline.question('Type a Wikipedia search term: ')
+    }
+
+    function askAndReturnPrefix() {
+        //select de opções que retorna uma chave como resultado(chave que foi selecionada pelo usuário)
+        const prefixes = ['Who is', 'What is', 'The history of']
+        //pega o indice do array
+        const selectedPrefixIndex = readline.keyInSelect(prefixes, 'Choose one option: ')
+        //pega o texto do indice
+        const selectedPrefixText = prefixes[selectedPrefixIndex]
+        //injeto o valor do texto na propriedade prefix
+        return selectedPrefixText
+    }
+
+    console.log(content)
+}
+
+start()
