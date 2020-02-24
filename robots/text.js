1a35c5e43bdef34a6f8f77f8dc8b5f169cb299d7
yesmin <ymslahoud@gmail.com>
2020-02-24 16:11:23 -0300

    feat: add Watson natural language understanding service

--- robots/text.js
+++ robots/text.js
@@ -2,10 +2,22 @@ const algorihtmia = require('algorithmia')
 const algorithmiaApiKey = require('../credentials/algorithmia.json').apiKey
 const sentencesBoundaryDetection = require('sbd')
 
+const watsonApiKey = require('../credentials/watson-nlu.json').apikey
+
+const NaturalLanguageUnderstandingV1 = require('watson-developer-cloud/natural-language-understanding/v1.js');
+
+var nlu = new NaturalLanguageUnderstandingV1({
+    iam_apikey: watsonApiKey,
+    version: '2018-04-05',
+    url: 'https://gateway.watsonplatform.net/natural-language-understanding/api/'
+});
+
 async function robot(content) {
     await fetchContentFromWikipedia(content) //baixa o conteúdo do Wikipedia
     sanitizeContent(content) //limpa o conteudo
     breakContentIntoSentences(content) //quebra em sentenças
+    limitMaximumSentences(content)
+    await fetchKeywordsOfAllSentences(content)
 
     console.log(fetchContentFromWikipedia())
 
@@ -15,9 +27,9 @@ async function robot(content) {
         //através da instância autenticada consegue-se chegar ao algoritmo por meio do método algo
         const wikipediaAlgotithm = algorihtmiaAuthenticated.algo('web/WikipediaParser/0.1.2')
         //o método pipe aceita por parâmetro o conteúdo que se deseja buscar no wikipedia
-        const wikipediaResponde = await wikipediaAlgotithm.pipe(content.searchTerm)
+        const wikipediaResponse = await wikipediaAlgotithm.pipe(content.searchTerm)
         //quando executa o método get(), o conteúdo do wikipedia cai dentro da variável wikipediaContent
-        const wikipediaContent = wikipediaResponde.get()
+        const wikipediaContent = wikipediaResponse.get()
         //console.log(wikipediaContent)
         content.sourceContentOriginal = wikipediaContent.content
     }
@@ -55,6 +67,37 @@ async function robot(content) {
                 text: sentence,
                 keywords: [],
                 images: []
+            })
+        })
+    }
+
+    function limitMaximumSentences(content) {
+        content.sentences = content.sentences.slice(0, content.maximumSentences)
+    }
+
+    async function fetchKeywordsOfAllSentences(content) {
+        for (const sentence of content.sentences) {
+            sentence.keywords = await fetchWatsonAndReturnKeywords(sentence.text)
+        }
+    }
+
+    async function fetchWatsonAndReturnKeywords(sentence) {
+        return new Promise((resolve, reject) => {
+            nlu.analyze({
+                text: sentence,
+                features: {
+                    keywords: {}
+                }
+            }, (error, response) => {
+                if (error) {
+                    throw error
+                }
+
+                const keywords = response.keywords.map((keyword) => {
+                    return keyword.text
+                })
+
+                resolve(keywords)
             })
         })
     }
